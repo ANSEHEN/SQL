b8adb4f0c99d579a62ff74f22245016bf7593ec0
EHKIM10 <esguthaben@gmail.com>
2017-06-26 13:02:40 +0900

    add 'image address' column

--- user_info.sql
+++ user_info.sql
@@ -2,6 +2,7 @@ create table USER_INFO (
 user_id varchar(12) not null,
 phone_num varchar(14) not null,
 password varchar(5) not null,
+image_add varchar(200) not null,
 primary key(user_id,phone_num)
 );
 
