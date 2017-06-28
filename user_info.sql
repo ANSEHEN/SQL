60e2d14726651c71132f17ae23c5ca0136c0e29d
EHKIM10 <esguthaben@gmail.com>
2017-06-28 10:23:58 +0900

    delete userid column and change primary key

--- user_info.sql
+++ user_info.sql
@@ -1,11 +1,10 @@
 create table USER_INFO (
-user_id varchar(12) not null,
 phone_num varchar(14) not null,
 phone_num_input varchar(14) not null,
 name varchar(20) not null,
 pw varchar(5) not null,
 image_add varchar(200) not null,
-primary key(user_id,phone_num)
+primary key(name,phone_num)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
