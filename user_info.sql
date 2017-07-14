90fb3e8a38d7436c9abc50ed8eb1fdbff9cd8264
EHKIM10 <esguthaben@gmail.com>
2017-07-14 10:34:05 +0900

    Update user_info.sql

--- user_info.sql
+++ user_info.sql
@@ -1,10 +1,11 @@
 create table USER_INFO (
+unique_key varchar(100) not null,
+name varchar(20) not null,
 phone_num varchar(14) not null,
 phone_num_input varchar(14) not null,
-name varchar(20) not null,
 pw varchar(5) not null,
 image_add varchar(200) not null,
-primary key(name,phone_num)
+primary key(unique_key)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
